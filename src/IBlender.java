ed5a7c25aba0c5d993cb214034a130a6e6152791
Jorge <mor24284@uvg.edu.gt>
2025-01-23 22:16:35 -0600

    Update IBlender.java

--- src/IBlender.java
+++ src/IBlender.java
@@ -1,8 +1,18 @@
+//Interfaz que define las operaciones basicas de la licuadora.
 public interface IBlender{
+    //incrementar velocidad
     void SpeedUp();
+    //reducir velocidad 
     void SpeedDown();
+    //llenar licuadora
     void Fill();
+    // vaciar licuadora
     void Empty();
+    /*
+    Verifica si la licuadora está llena.
+    return true si la licuadora está llena, false en caso contrario.
+     */
     boolean IsFull();
+    // devuelve la velocidad actual de la licuadora
     int GetSpeed();
 }
